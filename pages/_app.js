eedc17dab6bc59cae919864c90d68d02e6d38ea9
O2Soft <softcircles001@gmail.com>
2023-01-09 11:50:51 +0500

    add NextAuth lab and make a api authenticate with GitHub

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar/Navbar";
+import "../styles/globals.css";
+import "../components/Navbar/Navbar.css";
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
